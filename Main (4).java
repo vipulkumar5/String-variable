f74152134fcd5f57b9851cdbcc9feeb3a66e4a38
VIPUL KUMAR <54803468+vipulkumar5@users.noreply.github.com>
2021-12-13 00:45:23 +0530

    Add files via upload

---
+++ Main (4).java
@@ -0,0 +1,6 @@
+public class Main{
+    public static void main(String[]args){
+        String txt="ABCDEFGHIJKLMNOPQRSTUVWXYZ" ;
+        System.out.println("the length of the txt IS "+txt.length());
+    }
+}
